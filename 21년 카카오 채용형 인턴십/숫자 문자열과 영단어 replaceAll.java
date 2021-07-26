6e82a4ed3d9b3d5052e7c13c66ce7b4f39918123
jurlring <78091011+jurlring@users.noreply.github.com>
2021-07-26 15:19:44 +0900

    Create 숫자 문자열과 영단어 replaceAll.java
    replaceAll이용

---
+++ 21년 카카오 채용형 인턴십/숫자 문자열과 영단어 replaceAll.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder("");
+        int len = s.length();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i],digits[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+    public static void main(String[] args) {
+    }
+}
