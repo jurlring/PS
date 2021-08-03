dc4f40f1a3de68ca187746e8f359f1c48d64c985
jurlring <78091011+jurlring@users.noreply.github.com>
2021-08-03 14:43:05 +0900

    튜플 문자열 메서드를 사용하여 코드를 더 간결하게
    문자열 메서드를 사용하여 코드를 더 간결하게 할 수 있다.

---
+++ 2019 카카오 겨울 개발자 인턴십/튜플 문자열 메서드 이용.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class code {
+	public int[] solution(String s) {
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+            }
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+    }
+}
